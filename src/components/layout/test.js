977ca558aac3981be82675f0e29d16f935afa9c5
AG LOG <aggedda@gmail.com>
2019-11-30 15:49:01 +0300

    testing isssue

---
+++ src/components/layout/test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Lists from '../lists/Lists';
+
+const Index = () => {
+    return (
+        <React.Fragment>
+            <Lists/>
+        </React.Fragment>
+    )
+}
+
+export default Index;
